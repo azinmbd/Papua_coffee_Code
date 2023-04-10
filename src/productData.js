eba558fca798430b834a7919e71fc64f5adbee77
Azin <48130819+azinmbd@users.noreply.github.com>
2023-04-09 17:25:03 -0700

    final project code

---
+++ src/productData.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    title: "Dark coffee",
+    price: 7,
+    img: "/static/media/pack1.32daa2a43c2fc39edbe3.png",
+    quantity: 1,
+  },
+  {
+    id: 2,
+    title: "Vegan Coffee",
+    price: 6,
+    img: "/static/media/pack2.4d7566b78eb3f8331dba.png",
+    quantity: 1,
+  },
+  {
+    id: 3,
+    title: "Roasted Coffee",
+    price: 5,
+    img: "/static/media/pack1.32daa2a43c2fc39edbe3.png",
+    quantity: 1,
+  },
+  {
+    id: 4,
+    title: "Dark Roasted Coffee",
+    price: 4,
+    img: "/static/media/pack2.4d7566b78eb3f8331dba.png",
+    quantity: 1,
+  },
+  {
+    id: 5,
+    title: "Medium Roasted Coffee",
+    price: 4,
+    img: "/static/media/pack1.32daa2a43c2fc39edbe3.png",
+    quantity: 1,
+  },
+];
